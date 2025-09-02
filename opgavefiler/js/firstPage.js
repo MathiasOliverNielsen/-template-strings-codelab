603a4a09559912922eebac9bcf17f7ed9d99bbf1
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-09-02 08:44:37 +0200

    rettet opgave nummer

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -1,25 +1,17 @@
 /* Opgave 1*/
 
-
 // din kode her
 
-
-/* Opgave 1*/
-
+/* Opgave 2*/
 
 // din kode her
-
-
 
 /* Opgave 3 - skriv videre på koden her: */
 
 // array med person data
 const myPersons = [];
 
-
 // find form elementer
 const myForm = document.getElementById('formular');
 const submitButton = document.getElementById('indsend-knap');
 const personList = document.getElementById('person-liste');
-
-
